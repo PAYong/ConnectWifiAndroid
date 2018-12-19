62837b9cb67e4729a56400f4a2afa54cb0f4261b
PAYong <jamola.ronnafaye@ateamsolutions.co>
2018-12-19 17:27:45 +0800

    Sample POST with response.

--- app/src/main/java/com/example/android/connectwifi/DeviceInfo.java
+++ app/src/main/java/com/example/android/connectwifi/DeviceInfo.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,11 +14,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class DeviceInfo extends AppCompatActivity {
 
-    private TextView status;
-    private EditText password;
-    private Button connectBtn;
+    private TextView status, result;
+    private EditText password, urlPath;
+    private Button connectBtn, addDeviceBtn;
     private WifiManager wifiManager;
     String ssid;
 
@@ -35,6 +45,9 @@ public class DeviceInfo extends AppCompatActivity {
         password = findViewById(R.id.editTextPassword);
         connectBtn = findViewById(R.id.buttonConnect);
         status = findViewById(R.id.textViewStatus);
+        addDeviceBtn = findViewById(R.id.buttonAddDevice);
+        urlPath = findViewById(R.id.editTextURL);
+        result = findViewById(R.id.textViewResult);
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         if (!wifiManager.isWifiEnabled()) {
@@ -46,6 +59,13 @@ public class DeviceInfo extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 connectToWifi(password.getText().toString(), ssid);
+            }
+        });
+
+        addDeviceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AddDevice().execute(urlPath.getText().toString() , "data");
             }
         });
     }
@@ -75,6 +95,63 @@ public class DeviceInfo extends AppCompatActivity {
             Log.d(TAG, "Device is connected. netId: " +  netId);
         }else{
             Log.d(TAG, "ERROR");
+        }
+    }
+
+    public class AddDevice extends AsyncTask<String, String, Boolean> {
+
+        public AddDevice(){
+            //set context variables if required
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String urlString = params[0]; // URL to call
+            String data = params[1]; //data to post
+            OutputStream out = null;
+
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(15000);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+
+                out = new BufferedOutputStream(urlConnection.getOutputStream());
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+                writer.write(data);
+                writer.flush();
+                writer.close();
+                out.close();
+
+                urlConnection.connect();
+
+                // Check the connection response
+                InputStream  response = urlConnection.getInputStream();
+                InputStreamReader isw = new InputStreamReader(response);
+
+                int dataRes = isw.read();
+                StringBuilder sb = new StringBuilder();
+
+                while (dataRes != -1) {
+                    char current = (char) dataRes;
+                    dataRes = isw.read();
+                    sb.append(current);
+                }
+                Log.d(TAG, "response:" +  sb.toString());
+                result.setText(sb.toString());
+                return true;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            return false;
         }
     }
 }
